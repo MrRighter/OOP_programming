9e1c6e92cfe5e32270ba53026f6bb8d5036b7c72
Mr.Righter <kirill.a.shikunov@gmail.com>
2025-09-30 14:14:24 +0300

    update second practical work

---
+++ First practical work/1_Task1_(a_b_v).cpp
@@ -0,0 +1,60 @@
+﻿//#include <iostream>
+//#include <iomanip>  // для вывода таблицы
+//#include <string>  // для работы с текстом
+//#include <windows.h>
+//using namespace std;
+//
+//int main() {
+//    SetConsoleCP(1251);
+//    SetConsoleOutputCP(1251);
+//    setlocale(LC_ALL, "Russian");
+//
+//    const int USERS_COUNT = 3;
+//    const int DAYS_IN_MONTH = 30;
+//
+//    string surnames[USERS_COUNT];
+//    string names[USERS_COUNT];
+//    int years[USERS_COUNT];
+//    int months[USERS_COUNT];
+//
+//    // ввод данных для каждого пользователя
+//    for (int i = 0; i < USERS_COUNT; ++i) {
+//        cout << "\nПользователь " << (i + 1) << ":\n";
+//
+//        cout << "Введите фамилию: ";
+//        cin >> surnames[i];
+//
+//        cout << "Введите имя: ";
+//        cin >> names[i];
+//
+//        cout << "Введите полное кол-во лет: ";
+//        cin >> years[i];
+//
+//        cout << "Введите неполное кол-во лет в месяцах: ";
+//        cin >> months[i];
+//    }
+//
+//    // вывод таблицы
+//    cout << "\nРезультат:\n";
+//    cout << left
+//        << setw(20) << "Фамилия"
+//        << setw(20) << "Имя"
+//        << setw(20) << "Возраст (мес)"
+//        << setw(20) << "Возраст (дней)"
+//        << endl;
+//
+//    // вычисление и вывод данных для каждого пользователя
+//    for (int i = 0; i < USERS_COUNT; ++i) {
+//        int total_months = years[i] * 12 + months[i];  // общий возраст в месяцах
+//        int total_days = total_months * DAYS_IN_MONTH;  // общий возраст в днях
+//
+//        cout << left
+//            << setw(20) << surnames[i]
+//            << setw(20) << names[i]
+//            << setw(20) << total_months
+//            << setw(20) << total_days
+//            << endl;
+//    }
+//
+//    return 0;
+//}
---
+++ Second practical work/2_Task1_(a_b_v).cpp
@@ -0,0 +1,39 @@
+﻿//#include <iostream>
+//#include <string>
+//using namespace std;
+//
+//int main() {
+//	setlocale(0, "");
+//
+//	string user1_password = "12345";
+//	string user1_login = "peter";
+//	string user2_password = "67890";
+//	string user2_login = "kirill";
+//	int count = 3;
+//
+//    while (count > 0) {
+//        string input_login, input_password;
+//
+//        cout << "Осталось попыток: " << count << endl;
+//        cout << "Введите логин: ";
+//        cin >> input_login;
+//        cout << "Введите пароль: ";
+//        cin >> input_password;
+//
+//        if ((input_login == user1_login and input_password == user1_password) or
+//            (input_login == user2_login and input_password == user2_password)) {
+//            cout << "Успешный вход!" << endl;
+//            break;  // выходим из цикла при успехе
+//        }
+//        else {
+//            cout << "Ошибка! Неверный логин или пароль.\n" << endl;
+//            count--;  // уменьшаем количество попыток
+//        }
+//    }
+//
+//    if (count == 0) {
+//        cout << "Попытки закончились!" << endl;
+//    }
+//
+//	return 0;
+//}

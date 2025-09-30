0600013baf2e96a22b80b2b675790328e40e483b
Mr.Righter <kirill.a.shikunov@gmail.com>
2025-09-30 14:16:05 +0300

    update second practical work

--- Second practical work/2_Task2.cpp
+++ Second practical work/2_Task2.cpp
@@ -1,35 +1,35 @@
-﻿#include <iostream>
-using namespace std;
-
-int main() {
-    setlocale(0, "");
-
-    int count;
-    cout << "Сколько чисел вводить? ";
-    cin >> count;
-
-    if (count <= 0) {
-        cout << "Неверное количество!" << endl;
-        return 0;
-    }
-
-    int max_number;
-    cout << "Введите число 1: ";
-    cin >> max_number;  // первое число сразу становится максимумом
-
-    // обрабатываем остальные числа
-    for (int i = 2; i <= count; i++) {
-        int current_number;
-        cout << "Введите число " << i << ": ";
-        cin >> current_number;
-
-        // если текущее число больше максимума - обновляем максимум
-        if (current_number > max_number) {
-            max_number = current_number;
-        }
-    }
-
-    cout << "Наибольшее число: " << max_number << endl;
-
-    return 0;
-}
+﻿//#include <iostream>
+//using namespace std;
+//
+//int main() {
+//    setlocale(0, "");
+//
+//    int count;
+//    cout << "Сколько чисел вводить? ";
+//    cin >> count;
+//
+//    if (count <= 0) {
+//        cout << "Неверное количество!" << endl;
+//        return 0;
+//    }
+//
+//    int max_number;
+//    cout << "Введите число 1: ";
+//    cin >> max_number;  // первое число сразу становится максимумом
+//
+//    // обрабатываем остальные числа
+//    for (int i = 2; i <= count; i++) {
+//        int current_number;
+//        cout << "Введите число " << i << ": ";
+//        cin >> current_number;
+//
+//        // если текущее число больше максимума - обновляем максимум
+//        if (current_number > max_number) {
+//            max_number = current_number;
+//        }
+//    }
+//
+//    cout << "Наибольшее число: " << max_number << endl;
+//
+//    return 0;
+//}
